b36b5b90c7f8da0e0aa93764554b79e802e6d597
Alfianmuh12 <alfian.2001.16@gmail.com>
2023-05-31 22:20:59 +0700

    Add Clock and SectionPager in HomeFragment

--- app/src/main/java/com/example/ndarus/HomeFragment.kt
+++ app/src/main/java/com/example/ndarus/HomeFragment.kt
@@ -1,19 +1,48 @@
 package com.example.ndarus
 
+
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class HomeFragment : Fragment() {
+
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.tab_surah,
+            R.string.tab_Juz
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(requireActivity() as AppCompatActivity)
+        val viewPager: ViewPager2 = rootView.findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+
+        val tabs: TabLayout = rootView.findViewById(R.id.tabs)
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+
+
+        return rootView
     }
 }
